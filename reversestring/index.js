cfba53472f502d996b4d6a677033b63ba241cecf
lakshmydivyasharma <lakshmydivyasharma@users.noreply.github.com>
2020-06-12 14:25:14 -0400

    updated v

--- reversestring/index.js
+++ reversestring/index.js
@@ -12,19 +12,42 @@ function reverse(str) {
 
 module.exports = reverse;
 
-// function reverse(str) {
-//   return str
-//     .split('')
-//     .reverse()
-//     .join('');
-// }
+function reverse(str) {
+  return str
+    .split('')
+    .reverse()
+    .join('');
+}
 
-// function reverse(str) {
-//   let reversed = '';
-//
-//   for (let character of str) {
-//     reversed = character + reversed;
-//   }
-//
-//   return reversed;
-// }
+function reverse(str) {
+  let reversed = '';
+
+  for (let character of str) {
+    reversed = character + reversed;
+  }
+
+  return reversed;
+}
+
+
+// third way; alternaive sol'n if we dont use reverse
+
+//psuedocode:
+//create an emptty string called 'reversed'
+//for each character in the provided string
+// take the character and add it o the start of the reversed
+
+funcion reverse(str){
+  let reversed = '';
+
+  for (let character of str){ //itereate through each string
+    reversed = character + reversed;
+  }
+
+  return reversed;
+}
+
+// fourth way:
+funcion reverse(str){
+  return str.split('').reduce((reversed, character) => character + reversed, '')
+}
